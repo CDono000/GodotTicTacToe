4e03706ead7f8c34d42e34be92f7c6093fa0102a
cal <crowleon758@gmail.com>
2026-03-23 00:13:48 +0000

    The AI "works" but needs refining so that it stops the player from winning

--- game.gd
+++ game.gd
@@ -6,8 +6,9 @@ extends Node2D
 @onready var buttons = $GridContainer.get_children()
 
 #Game States
-var currentPlayer
+var currentPlayer = "X"
 var board
+var AI = false
 
 func _ready() -> void:
 	var index = 0
@@ -16,40 +17,106 @@ func _ready() -> void:
 		index+=1
 
 func onClick(index, button):
+	place(index, button)
+	if checkWin(board) == 0 and !checkFull(board) and AI:
+		var bestMove = -1
+		var bestScore = INF
+		for AIIndex in range(9):
+			if board[AIIndex/3][AIIndex%3] == "":
+				board[AIIndex/3][AIIndex%3] = "O"
+				var score = search(9, board.duplicate(true), true)
+				board[AIIndex/3][AIIndex%3] = ""
+				if score < bestScore:
+					bestScore = score
+					bestMove = AIIndex
+		place(bestMove, buttons[bestMove])
+
+func place(index, button):
 	board[index / 3][index % 3] = currentPlayer
 	button.text = currentPlayer
 	button.disabled = true
-	if checkWin() or checkFull():
-		if checkFull():
+	if checkWin(board) != 0 or checkFull(board):
+		if checkWin(board) == 0 and checkFull(board):
 			$Menu/Label.text = "DRAW"
 		else:
 			$Menu/Label.text = currentPlayer + "'s Win"
 		$Menu.show()
 	currentPlayer = "X" if currentPlayer == "O" else "O"
 
-func checkWin():
+func checkWin(checkBoard: Array):
+	var dict = {"X":1, "O":-1, "":0}
 	for i in range(3):
-		if board[i][0] == board[i][1] and  board[i][1] == board[i][2] and board[i][2] != "":
-			return true
-		if board[0][i] == board[1][i] and  board[1][i] == board[2][i] and board[2][i] != "":
-			return true
-	if board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[2][2] != "":
-		return true
-	if board[2][0] == board[1][1] and board[1][1] == board[0][2] and board[0][2] != "":
-		return true
-	return false
+		if checkBoard[i][0] == checkBoard[i][1] and  checkBoard[i][1] == checkBoard[i][2] and checkBoard[i][2] != "":
+			return dict[checkBoard[i][2]]
+		if checkBoard[0][i] == checkBoard[1][i] and  checkBoard[1][i] == checkBoard[2][i] and checkBoard[2][i] != "":
+			return dict[checkBoard[2][i]]
+	if checkBoard[0][0] == checkBoard[1][1] and checkBoard[1][1] == checkBoard[2][2] and checkBoard[2][2] != "":
+		return dict[board[2][2]]
+	if checkBoard[2][0] == checkBoard[1][1] and checkBoard[1][1] == checkBoard[0][2] and checkBoard[0][2] != "":
+		return dict[checkBoard[0][2]]
+	return 0
 
-func checkFull():
+func checkFull(checkBoard):
 	for row in range(3):
 		for col in range(3):
-			if board[row][col] == "":
+			if checkBoard[row][col] == "":
 				return false
 	return true
 
+func search(depth: int, searchBoard: Array, isMax: bool):
+	var best
+	if checkWin(searchBoard) != 0 or checkFull(searchBoard) or depth == 0:
+		return evaluate(searchBoard)
+	if isMax:
+		best = -INF
+		for index in range(9):
+			if searchBoard[index/3][index%3] == "":
+				searchBoard[index/3][index%3] = "X"
+				var score = search(depth-1, searchBoard.duplicate(true), false)-10
+				best = max(score, best)
+	else:
+		best = INF
+		for index in range(9):
+			if searchBoard[index/3][index%3] == "":
+				searchBoard[index/3][index%3] = "X"
+				var score = search(depth-1, searchBoard.duplicate(true), true)-10
+				best = min(score, best)
+	return best
+
+func evaluate(evalBoard):
+	var dict = {"X":1, "O":-1, "":0}
+	var win = checkWin(evalBoard)
+	var score = 0
+	if win == -1:
+		return -500
+	elif win == 1:
+		return 1000
+	score += 7 * dict[board[0][0]]
+	score += 7 * dict[board[2][0]]
+	score += 7 * dict[board[2][2]]
+	score += 7 * dict[board[0][2]]
+	score += 5 * dict[board[1][1]]
+	return score
+
 func resetGame():
-	currentPlayer = "X"
 	board = [["", "", ""], ["", "", ""], ["", "", ""]]
 	for button in buttons:
 		button.text = ""
 		button.disabled = false
 	$Menu.hide()
+	if currentPlayer == "O" and AI:
+		var bestMove = -1
+		var bestScore = INF
+		for AIIndex in range(9):
+			if board[AIIndex/3][AIIndex%3] == "":
+				board[AIIndex/3][AIIndex%3] = "O"
+				var score = search(9, board.duplicate(true), true)
+				board[AIIndex/3][AIIndex%3] = ""
+				if score < bestScore:
+					bestScore = score
+					bestMove = AIIndex
+		place(bestMove, buttons[bestMove])
+
+func _on_ai_pressed() -> void:
+	AI = true
+	resetGame()
